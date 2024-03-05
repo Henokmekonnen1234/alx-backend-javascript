3dbbfde486b1c6399952cb61c0d84dc8f4d60927
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-03-05 18:59:02 +0300

    task 1 completed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
   if (trueOrFalse) {
-    let task = false;
-E   let task2 = true;
+    const task = false;
+    const task2 = true;
   }
 
   return [task, task2];
