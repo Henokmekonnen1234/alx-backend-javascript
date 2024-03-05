d5fbf0ddf315685a7a5b225df5f05804712fc626
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-03-05 18:53:33 +0300

    task 1 completed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,9 @@
 export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
-  //eslint-disable-next-line no-empty
   if (trueOrFalse) {
-    
+    let task = false;
+E   let task2 = true;
   }
 
   return [task, task2];
