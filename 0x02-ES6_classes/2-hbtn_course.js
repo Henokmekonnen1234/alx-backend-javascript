94326f22e0ff47750b8d37758cc119f994fd5439
Henokmekonnen1234 <henokmac2425@gmail.com>
2024-03-07 13:30:56 +0300

    campleted task

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+function _validateName(value) {
+  if (typeof value !== 'string') {
+    throw new TypeError('Name must be a string');
+  }
+  return value;
+}
+
+function _validateLength(value) {
+  if (typeof value !== 'number') {
+    throw new TypeError('Length must be a number');
+  }
+  return value;
+}
+
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = _validateName(name);
+    this._length = _validateLength(length);
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    this._name = _validateName(name);
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    this._length = _validateLength(length);
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(student) {
+    this._students = student;
+  }
+}
